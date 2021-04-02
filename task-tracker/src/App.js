1c86f0548a1dfcd80b40bf30a5f9391034ecb9ba
Alexander Garuba <alexandergaruba@gmail.com>
2021-04-02 15:25:12 -0400

    TT4: Task component & Delete functionality
    Task.js component handles displaying information of each task. This is passes down from App -> Tasks -> Task
    
    onDelete, in App.js, handles the deletion of a Task. Upon a deletion click, this is passed up from Task -> Tasks -> App, where App changes the state with setTasks. This deletion is based on the id of the task from the UI.
    *on refresh, everything is back to normal because data is static in App.js

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -2,7 +2,7 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 import {useState} from 'react'
 
-function App() {
+const App = () => {
   const [tasks, setTasks] = useState([
 		{
 			id: 1,
@@ -18,10 +18,19 @@ function App() {
 		},
 	])
 
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+  
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No Tasks To Complete.')}
     </div>
   );
 }
